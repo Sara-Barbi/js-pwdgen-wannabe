8b2ed1fe7e9abe26f44f0d8808d48d7b534e6732
Sara-Barbi <barbisara02@gmail.com>
2021-12-02 14:54:04 +0100

    password

---
+++ myscript.js
@@ -0,0 +1,11 @@
+let myName = prompt("Inserisci Nome");
+let mySurname = prompt ("Inserisci Cognome");
+let myColor = prompt ("inserisci il tuo colore preferito")
+let myNumber = 21
+
+
+document.getElementById("allinfo").innerHTML = myName + mySurname + myColor + myNumber
+
+
+
+
