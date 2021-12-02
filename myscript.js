7363d3a4d26241f3328bf3c30ddb2bd053ea4c1b
Sara-Barbi <barbisara02@gmail.com>
2021-12-02 15:12:51 +0100

    creazionedella password

--- myscript.js
+++ myscript.js
@@ -3,7 +3,6 @@ let mySurname = prompt ("Inserisci Cognome");
 let myColor = prompt ("inserisci il tuo colore preferito")
 let myNumber = 21
 
-
 document.getElementById("allinfo").innerHTML = myName + mySurname + myColor + myNumber
 
 
